bdd6b78ba49020918e6f93d6422d4fc3daa4f1ff
Preyas <preyas25@gmail.com>
2026-05-04 22:52:18 -0700

    feat: add created_at column to models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,12 +1,14 @@
-from sqlalchemy import Column, String, Numeric
+from sqlalchemy import Column, String, Numeric, DateTime
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
 import uuid
-from database import Base
+from app.database import Base
 
 class Certificate(Base):
     __tablename__ = "certificates"
 
-    id = Column(UUID(as_uuid=True), primar_key=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
     provider = Column(String, index=True)
     course_name = Column(String)
     completion_date = Column(String) # String for v0, update to date with error handling in v1
